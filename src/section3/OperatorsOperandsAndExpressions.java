0952325c67cdeea3dc73d29326dce9b36c39c287
SerguioL <serguio.l.landa@gmail.com>
2022-01-29 22:06:26 -0600

    section 3

---
+++ src/section3/OperatorsOperandsAndExpressions.java
@@ -0,0 +1,17 @@
+package section3;
+
+public class OperatorsOperandsAndExpressions {
+
+    //What are Operators?
+
+    //Operators in Java special symbols that perform specific operations on one, two, or three operands, and then return a result. As an example, we used the + (addition) operator to sum the value of two variables in a previous video.
+
+    
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
